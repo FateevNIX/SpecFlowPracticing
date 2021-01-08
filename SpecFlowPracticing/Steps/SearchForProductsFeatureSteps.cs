d9d5264812088eb4806c275c7e1da72db2bbd59e
FateevNIX <aleksandr.fateev@nixsolutions.com>
2021-01-08 18:22:52 +0200

    add some Scenario Context examples;

--- SpecFlowPracticing/Steps/SearchForProductsFeatureSteps.cs
+++ SpecFlowPracticing/Steps/SearchForProductsFeatureSteps.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
 namespace SpecFlowPracticing.Steps
@@ -21,6 +22,47 @@ namespace SpecFlowPracticing.Steps
         public void WhenIEnterTheProductNameIntoSearchInput(string productName)
         {
            driver.FindElement(By.Id("search_query_top")).SendKeys(productName);
+
+            //все что ниже в степе, пока просто примеры работы c ScenarioContext
+            ScenarioContext.Current["InfoForNextStep"] = "Step 2 passed";
+            Console.WriteLine(ScenarioContext.Current["InfoForNextStep"].ToString());
+
+            List<ProductDetails> products = new List<ProductDetails>()
+            {
+                new ProductDetails()
+                {
+                    Name = "Blouse",
+                    Price = 100,
+                    Quantity = 2,
+                    Size = "M"
+                 },
+                 new ProductDetails()
+                {
+                    Name = "Dress",
+                    Price = 500,
+                    Quantity = 4,
+                    Size = "L"
+                 }
+            };
+
+            //сохраняем в Scenario Context
+            ScenarioContext.Current.Add("Products", products);
+
+            //Достаем значение из Scenario Context
+            var producList = ScenarioContext.Current.Get<IEnumerable<ProductDetails>>("Products");
+            foreach (ProductDetails product in producList)
+            {
+                Console.WriteLine("Product Name is: " + product.Name);
+                Console.WriteLine("Product Price is: " + product.Price);
+                Console.WriteLine("Product Quantity is: " + product.Quantity);
+                Console.WriteLine("Product Size is: " + product.Size);
+            }
+
+            //Выводит тайтл самого сценария
+            Console.WriteLine(ScenarioContext.Current.ScenarioInfo.Title);
+
+            //Выводит тип блока 
+            Console.WriteLine(ScenarioContext.Current.CurrentScenarioBlock);
         }
         
         [When(@"Click on search button")]
