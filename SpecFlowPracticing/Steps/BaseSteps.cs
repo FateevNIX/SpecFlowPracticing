7304c1c18fe4dc0afbc3d0ddf8e91fe88e4a7c14
FateevNIX <aleksandr.fateev@nixsolutions.com>
2021-01-11 18:46:06 +0200

    create BaseSteps class

---
+++ SpecFlowPracticing/Steps/BaseSteps.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowPracticing.Steps
+{
+    public class BaseSteps
+    {
+       public IWebDriver driver = new ChromeDriver();
+    }
+}
