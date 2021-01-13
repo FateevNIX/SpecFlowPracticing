a6b38f42ec41bbd171ca8de1c21e69f2ad2033a6
FateevNIX <aleksandr.fateev@nixsolutions.com>
2021-01-13 18:50:25 +0200

    Add custom waiter which takes IWebElement;

---
+++ SpecFlowPracticing/Utils/Waiter.cs
@@ -0,0 +1,22 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace SpecFlowPracticing.Utils
+{
+    public class Waiter
+    {
+        public static void wait(IWebDriver driver, IWebElement element)
+        {
+            DefaultWait<IWebDriver> wait = new DefaultWait<IWebDriver>(driver);
+            wait.Timeout = TimeSpan.FromMinutes(1);
+            wait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            wait.Until(ExpectedConditions.ElementToBeClickable(element));
+          
+          
+        }
+    }
+}
