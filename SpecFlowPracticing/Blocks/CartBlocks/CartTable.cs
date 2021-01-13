0f7747335eb4f531a1fe79f42c3f52d5cadff9ce
FateevNIX <aleksandr.fateev@nixsolutions.com>
2021-01-13 18:51:50 +0200

    create blocks for ProductDetailspage and CartPage

---
+++ SpecFlowPracticing/Blocks/CartBlocks/CartTable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPracticing.Blocks.CartBlocks
+{
+    [Binding]
+    class CartTable
+    {
+    }
+}
---
+++ SpecFlowPracticing/Blocks/ProductDetailsBlocks/AddToCartModal.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using SpecFlowPracticing.Pages;
+using SpecFlowPracticing.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPracticing.Blocks
+{
+    [Binding]
+    public class AddToCartModal
+    {
+        private IWebDriver driver;
+        public AddToCartModal(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this); 
+        }
+
+        [FindsBy(How = How.XPath, Using = ".//a[@title= 'Proceed to checkout']")]
+        protected IWebElement proceedToCheckoutButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//a[@title= 'Continue shopping']")]
+        protected IWebElement continueShoppingButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = ".//div[@id='layer_cart']//div[contains(@class,'layer_cart_product')]/h2")]
+        protected IWebElement modalTitle { get; set; }
+
+        public void successfullyAddedModalIsShown()
+        {
+            Waiter.wait(driver, proceedToCheckoutButton);
+            Assert.AreEqual(modalTitle.Text, "Product successfully added to your shopping cart");
+        }
+    }
+}
