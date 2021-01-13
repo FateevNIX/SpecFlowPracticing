b60df44c3864f7212752b7556b5adda89797868c
FateevNIX <aleksandr.fateev@nixsolutions.com>
2021-01-13 18:53:09 +0200

    added CartPage and CartPageSteps classes

---
+++ SpecFlowPracticing/Pages/CartPage.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpecFlowPracticing.Pages
+{
+   public class CartPage
+    {
+        private IWebDriver driver;
+        public CartPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+    }
+}
---
+++ SpecFlowPracticing/Steps/CartPageSteps.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using SpecFlowPracticing.Pages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowPracticing.Steps
+{
+    [Binding]
+   public class CartPageSteps
+    {
+        private CartPage CartPage { get; }
+        public CartPageSteps(IWebDriver driver)
+        {
+            CartPage = new CartPage(driver);
+        }
+    }
+}
